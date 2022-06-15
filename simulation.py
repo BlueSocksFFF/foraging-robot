0afc1cf61c30fced8386a345e564b92516aa2842
Diane Zhang <yizhan22@colby.edu>
2022-06-14 22:48:38 -0400

    Added files

---
+++ simulation.py
@@ -0,0 +1,5 @@
+from model import *
+
+model = InitialModel(10)
+for i in range(10):
+    model.step()
---
+++ visualization.py
@@ -0,0 +1,21 @@
+from model import *
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Color": "red",
+                 "Filled": "true",
+                 "Layer": "0",
+                 "r": 0.5}
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+
+server = ModularServer(InitialModel,
+                       [grid],
+                       "First Model Visualization",
+                       {"N":1, "width":10, "height":10})
+
+#server.port = 8521 #default
+server.launch()
