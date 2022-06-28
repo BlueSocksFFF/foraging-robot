6b250221a188b5888f8e02553924cb2e22663402
Diane Zhang <yizhan22@colby.edu>
2022-06-28 12:11:06 -0400

    Changed name

--- job_division/scouter.py
+++ job_division/scouter.py
@@ -1,5 +1,7 @@
 import mesa
+from random_foraging import Forager
 
-class Scouter(mesa.Agent):
-    
-    
+class Scouter(Forager):
+
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
---
+++ random_foraging/__init__.py
@@ -0,0 +1,23 @@
+"""
+Random Foraging Model
+
+Core Objects: Forager, Food, and Obstacle
+"""
+
+import mesa
+
+from forager import Forager
+from food import Food
+from obstacle import Obstacle
+import grid as grid
+from model import RandomForagingModel
+
+__all__ = [
+    "Forager",
+    "Food",
+    "Obstacle",
+    "grid",
+    "RandomForagingModel"
+]
+
+__title__ = "random_foraging"
